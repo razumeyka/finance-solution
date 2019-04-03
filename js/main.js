47cad2d0bf3493ff39977cbe24206f1949f8cebb
razumeyka <33911265+razumeyka@users.noreply.github.com>
2019-04-04 00:09:08 +0300

    finished

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+	
+// menu
+	
+	$(function() {
+		$('.smooth').on('click', function(event) {
+			var target = $(this.getAttribute('href'));
+			if (target.length) {
+				event.preventDefault();
+				$('html, body').stop().animate({
+					scrollTop: target.offset().top
+				}, 1000);
+			}
+		});
+	});
+	
+	$('.nav-item').click(function(){
+        $(".navbar-nav").find(".nav-item").removeClass("active");
+		$(this).addClass("active");
+    });
+	
     
 // question-answer
 	
@@ -12,5 +32,23 @@ $(document).ready(function(){
 	
     $('input[type="tel"]').mask("8-999-999-99-99");
 	
+// carousel 
+	
+	$('.carousel_team .carousel-item').each(function(){
+		var next = $(this).next();
+		if (!next.length) {
+		next = $(this).siblings(':first');
+		}
+		next.children(':first-child').clone().appendTo($(this));
+
+		for (var i=0;i<3;i++) {
+			next=next.next();
+			if (!next.length) {
+				next = $(this).siblings(':first');
+			}
+
+			next.children(':first-child').clone().appendTo($(this));
+		  }
+	});
 	
 }); 
